5306052d67c1505ad3076c0c766c57bdce91121e
Claudiu Nedelcu <claudiu.nedelcu.m@gmail.com>
2019-12-13 19:50:55 +0200

    fix paranteze

--- app.py
+++ app.py
@@ -62,6 +62,8 @@ def makeCmd(_name, _text):
     print(cmds)
     for cmd in cmds:
         cmd = cmd.replace(';', '')
+        cmd = cmd.replace('(', ' ( ')
+        cmd = cmd.replace(')', ' ) ')
         cmd.lower()
         cmd = re.sub(' +', ' ', cmd.strip())
         textList = cmd.split()
